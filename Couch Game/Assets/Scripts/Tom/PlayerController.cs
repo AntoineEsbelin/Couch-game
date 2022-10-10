fa746083cdf568084cd463646f12b74786bc203b
AkuroP <thomas.kang2002@gmail.com>
2022-10-10 22:26:26 +0200

    Counter v1
    + Counter
    + Player layer
    + Counter input action

--- Couch Game/Assets/Scripts/Tom/PlayerController.cs
+++ Couch Game/Assets/Scripts/Tom/PlayerController.cs
@@ -14,15 +14,26 @@ public class PlayerController : MonoBehaviour
     public References refs;
 
     bool isMoving;
+    [SerializeField] private bool isSpinning;
+    [SerializeField] private float stunedTime;
 
     #region Updates
 
         void FixedUpdate()
         {
-            isMoving = input.move != Vector2.zero;
+            if(stunedTime <= 0)
+            {
+                isMoving = input.move != Vector2.zero;
 
-            Movement();
-            ApplyMovement();
+                Movement();
+                ApplyMovement();
+                Countering();
+            }
+            else
+            {
+                stunedTime -= Time.deltaTime;
+                //refs.rb.velocity = Vector3.zero;
+            }
         }
 
     #endregion
@@ -73,6 +84,7 @@ public class PlayerController : MonoBehaviour
         [System.Serializable] public class Inputs
         {
             public Vector2 move;
+            public bool counter;
         }
 
         [HideInInspector] public Inputs input;
@@ -93,5 +105,102 @@ public class PlayerController : MonoBehaviour
             Debug.Log("quick spin");
         }
 
+        private void OnCounter(InputValue v)
+        {
+            if(v.isPressed)input.counter = true;
+
+            if(!counter.prepCounter)
+            {
+                counter.counterPrepTime = counter.maxCounterTime;
+                counter.prepCounter = true;
+            }
+        }
+
     #endregion
-}
+
+
+    #region Counter
+        [System.Serializable] public class CounterSettings
+        {
+            public bool prepCounter;
+            public float counterPrepTime;
+            public float maxCounterTime = 2f;
+            public float counteringTime;
+            public float maxCounteringTime = .5f;
+            public bool isCountering;
+
+            [Header("Hitbox")]
+            public Transform counterPos;
+            public float size;
+            public float maxDistance;
+            public LayerMask playerLM;
+
+            public float counterForce;
+        }
+
+        public CounterSettings counter;
+        private RaycastHit hit;
+
+        private void Countering()
+        {
+            //Prepare to counter
+            if(counter.prepCounter)
+            {
+                if(counter.counterPrepTime > 0)
+                {
+                    counter.counterPrepTime -= Time.deltaTime * counter.maxCounterTime;
+                    //refs.rb.velocity = Vector3.zero;
+                    //Debug.Log("COUNTERING INCOMING");
+                }
+                else
+                {
+                    input.counter = false;
+                    counter.prepCounter = false;
+                    counter.isCountering = true;
+                    counter.counteringTime = counter.maxCounteringTime;
+                    Debug.Log("START COUNTERING");
+                }
+            }
+
+            //is actually countering
+            if(counter.isCountering)
+            {
+                if(counter.counteringTime > 0)
+                {
+                    bool hitted = Physics.Raycast(counter.counterPos.position, counter.counterPos.forward, out hit, counter.maxDistance, counter.playerLM);
+                    if(hitted)
+                    {
+                        //Debug.Log("HIT SOMETHING");
+                        //Debug.Log(hit.collider.name);
+                        //Debug.Log("PLAYER COUNTERED");
+                        PlayerController enemyPlayer = hit.collider.GetComponent<PlayerController>();
+                        if(enemyPlayer.isSpinning)
+                        {
+                            Debug.Log("PLAYER STUNNED");
+                            enemyPlayer.stunedTime = 2f;
+                        }
+                        else
+                        {
+                            Debug.Log("PLAYER REPOUSSED");
+                            enemyPlayer.refs.rb.AddForce(-(transform.position - enemyPlayer.transform.position).normalized * counter.counterForce, ForceMode.Impulse);
+                        }
+                    }
+                    counter.counteringTime -= Time.deltaTime;
+                }
+                else
+                {
+                    counter.isCountering = false;
+                    Debug.Log("STOP COUNTERING");
+                }
+            }
+        }
+    #endregion
+
+    #region Gizmos
+
+    private void OnDrawGizmos()
+    {
+        if(counter.counterPos != null)Gizmos.DrawRay(counter.counterPos.position, counter.counterPos.forward * counter.maxDistance);
+    }
+    #endregion
+}   
