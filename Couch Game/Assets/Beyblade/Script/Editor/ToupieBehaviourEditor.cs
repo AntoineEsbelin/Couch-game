a41f2a389b1d43cad105b7641729930dbd4ad6e6
Romain <mromain911@gmail.com>
2022-10-10 10:48:29 +0200

    custom editor

---
+++ Couch Game/Assets/Beyblade/Script/Editor/ToupieBehaviourEditor.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ToupieBehaviour))]
+public class ToupieBehaviourEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        ToupieBehaviour playerB = (ToupieBehaviour) target;
+        
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("param"));
+
+        switch (playerB.param)
+        {
+            case ToupieBehaviour.StateParam.MOVE:
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("moveParam"));
+                break;
+            
+            case ToupieBehaviour.StateParam.CHARGE:
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("chargeParam"));
+                break;
+        }
+        
+        EditorGUILayout.Space(10);
+        playerB.controller = (CharacterController)EditorGUILayout.ObjectField("Character Controller", 
+            playerB.controller, typeof(CharacterController), true);
+        
+        EditorGUILayout.Space(10);
+        EditorGUILayout.LabelField("GroundCheck",EditorStyles.boldLabel);
+        EditorGUILayout.Space(3);
+        playerB.groundCheck = (Transform)EditorGUILayout.ObjectField("Character Controller", 
+            playerB.groundCheck, typeof(Transform), true);
+        
+        playerB.groundDistance = EditorGUILayout.Slider("GroundCheck Distance",playerB.groundDistance, 0f, 1f);
+        
+        playerB.groundMask = EditorGUILayout.LayerField("Ground Layer",playerB.groundMask);
+        
+        EditorGUILayout.Space(10);
+        playerB.repulseForce = EditorGUILayout.Slider("Repulse Force",playerB.repulseForce, .1f, 10f);
+        
+        
+        serializedObject.ApplyModifiedProperties();
+        
+        
+    }
+}
--- Couch Game/Assets/Beyblade/Script/ToupieBehaviour.cs
+++ Couch Game/Assets/Beyblade/Script/ToupieBehaviour.cs
@@ -5,38 +5,55 @@ using UnityEngine.InputSystem;
 
 public class ToupieBehaviour : MonoBehaviour
 {
+    
+    
     [Header("Component")]
     public CharacterController controller;
-    public Rigidbody rb;
 
     [Header("Input")]
     public PlayerControll playerControl;
     private InputAction charge;
 
-    [Header("Mouvement")]
-    public float speed = 6f;
-    public float turnSmoothTime = 0.1f;
-    public float turnSmoothVelocity;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3f;
+    public StateParam param;
+    public enum StateParam
+    {
+        MOVE,
+        CHARGE
+    }
+    
+    public MouvementParam moveParam;
+    [System.Serializable]
+    public class MouvementParam
+    {
+        [Range(0, 10f)]public float speed = 6f;
+        [Range(.01f, .5f)]public float turnSmoothTime = 0.1f;
+        public float gravity = -9.81f;
+        [Range(.1f, 5f)]public float jumpHeight = 3f;
+    } 
+    
+    private float turnSmoothVelocity;
     private Vector3 velocity;
-    private Vector3 impact = Vector3.zero;
     private bool playerHit;
-    public float impactForce;
     private Vector3 reflect;
     private Vector3 direction;
     private float jumpInput;
+
+    public float repulseForce = 10f;
     
 
-    [Header("Charge")]
-    public float chargeSpeed;
-    private bool chargeState;
+    public ChargeParam chargeParam;
+    [System.Serializable]
+    public class ChargeParam
+    {
+        [Range(3, 15f)]public float chargeSpeed;
+        [HideInInspector]public bool chargeState;
+    }
     
-
+    [Space(10)]
     [Header("GroundCheck")] 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
-    public LayerMask groundMask;
+    public LayerMask groundMask = 3;
     private bool isGrounded;
 
     private void Awake()
@@ -72,14 +89,14 @@ public class ToupieBehaviour : MonoBehaviour
     private void Rush(InputAction.CallbackContext obj)
     {
 
-        chargeState = true;
-        speed = 6;
+        chargeParam.chargeState = true;
+        moveParam.speed = 6;
 
     }
 
     private void PreCharge(InputAction.CallbackContext obj)
     {
-        speed = 0;
+        moveParam.speed = 0;
     }
 
     
@@ -88,21 +105,23 @@ public class ToupieBehaviour : MonoBehaviour
     {
         
         
-        if (direction.magnitude >= 0.1f && chargeState == false)
+        if (direction.magnitude >= 0.1f && chargeParam.chargeState == false)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, 
+                ref turnSmoothVelocity, moveParam.turnSmoothTime);
             
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             
-            controller.Move(moveDir * speed * Time.fixedDeltaTime);
+            controller.Move(moveDir * moveParam.speed * Time.fixedDeltaTime);
             
         }
     }
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
@@ -110,12 +129,13 @@ public class ToupieBehaviour : MonoBehaviour
 
         if (jumpInput > 0 && isGrounded)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            velocity.y = Mathf.Sqrt(moveParam.jumpHeight * -2f * moveParam.gravity);
         }
         
-        velocity.y += gravity * Time.deltaTime;
+        velocity.y += moveParam.gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-        if (chargeState)
+        
+        if (chargeParam.chargeState)
         {
             StartCoroutine(Rushing());
         }
@@ -130,7 +150,7 @@ public class ToupieBehaviour : MonoBehaviour
 
     IEnumerator Repulsion()
     {
-        controller.SimpleMove(reflect * 10);
+        controller.SimpleMove(reflect * repulseForce);
         yield return new WaitForSeconds(0.5f);
         playerHit = false;
     }
@@ -138,37 +158,40 @@ public class ToupieBehaviour : MonoBehaviour
     IEnumerator Rushing()
     {
         Vector3 forward = transform.TransformDirection(Vector3.forward);
-        controller.SimpleMove(forward * chargeSpeed);
+        controller.SimpleMove(forward * chargeParam.chargeSpeed);
         yield return new WaitForSeconds(1f);
-        chargeState = false;
+        chargeParam.chargeState = false;
     }
     
-   
-    private Vector3  AddImpact(Vector3 dir, float force)
-    {
-        Vector3 repulse;
-        dir.Normalize();
-        if (dir.y < 0) dir.y = -dir.y; // reflect down force on the ground
-        repulse = dir.normalized * force / rb.mass;
-        return repulse;
-    }
 
     private void OnCollisionEnter(Collision coll)
     {
         if (coll.collider.CompareTag("Wall"))
         {
-            print("hit");
-            if (chargeState)
-                chargeState = false;
+            if (chargeParam.chargeState)
+                chargeParam.chargeState = false;
+            
             playerHit = true;
             
             reflect = Quaternion.AngleAxis(180, coll.contacts[0].normal) * transform.forward * -1;
             reflect.Normalize();
-            
-            
 
         }
         
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (isGrounded)
+        {
+            Gizmos.color = Color.green;
+        }
+        else if (isGrounded == false)
+        {
+            Gizmos.color = Color.red;
+        }
+        
+        Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
     }
     
 }
