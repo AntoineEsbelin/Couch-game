f3764941fc297973dae1e02a9be0e7560ed4c303
Romain <mromain911@gmail.com>
2022-10-11 16:54:48 +0200

    tkt

--- Couch Game/Assets/Scripts/Thomas/PointZone.cs
+++ Couch Game/Assets/Scripts/Thomas/PointZone.cs
@@ -12,31 +12,31 @@ public class PointZone : MonoBehaviour
     {
         if(coll.CompareTag("Player"))
         {
-            // PlayerController deadPlayer = coll.GetComponent<PlayerController>();
-            // //Give a certain amount of point at the last player touched
-            // if(deadPlayer.playersInteract.lastPlayerContact != null)
-            // {
-            //     deadPlayer.playersInteract.lastPlayerContact.playersInteract.playerPoint += pointGiven;
-            //
-            //     //DEBUG
-            //     Debug.Log($"{deadPlayer.name} EJECTED !");
-            //     Debug.Log($"GIVE {pointGiven} points to {deadPlayer.playersInteract.lastPlayerContact.name}");
-            //
-            //     if(GameManager.instance.drawTimer)
-            //     {
-            //         if(deadPlayer.playersInteract.lastPlayerContact.playersInteract.playerPoint > GameManager.instance.drawMaxPoint)
-            //         {
-            //             Debug.Log($"{deadPlayer.playersInteract.lastPlayerContact.name} WINNN");
-            //             GameManager.instance.timeOut = true;
-            //             //STOP THE ROUND
-            //         }
-            //     }
-            // }
-            // else
-            // {
-            //     //DEBUG
-            //     Debug.Log($"{deadPlayer.name} SUICIDED !");
-            // }
+            PlayerController deadPlayer = coll.GetComponent<PlayerController>();
+            //Give a certain amount of point at the last player touched
+            if(deadPlayer.playersInteract.lastPlayerContact != null)
+            {
+                deadPlayer.playersInteract.lastPlayerContact.playersInteract.playerPoint += pointGiven;
+            
+                //DEBUG
+                Debug.Log($"{deadPlayer.name} EJECTED !");
+                Debug.Log($"GIVE {pointGiven} points to {deadPlayer.playersInteract.lastPlayerContact.name}");
+
+                if(GameManager.instance.drawTimer)
+                {
+                    if(deadPlayer.playersInteract.lastPlayerContact.playersInteract.playerPoint > GameManager.instance.drawMaxPoint)
+                    {
+                        Debug.Log($"{deadPlayer.playersInteract.lastPlayerContact.name} WINNN");
+                        GameManager.instance.timeOut = true;
+                        //STOP THE ROUND
+                    }
+                }
+            }
+            else
+            {
+                //DEBUG
+                Debug.Log($"{deadPlayer.name} SUICIDED !");
+            }
             //MORT DU JOUEUR TOMBÉ
             if (isController)
             {
