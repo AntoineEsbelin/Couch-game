063626bd6a1ec3df38aee76544052cba8dcbfa84
AkuroP <thomas.kang2002@gmail.com>
2022-10-26 10:50:41 +0200

    tout pareil

--- Couch Game/Assets/Scripts/Toupie-Mecha/BounceWall.cs
+++ Couch Game/Assets/Scripts/Toupie-Mecha/BounceWall.cs
@@ -7,10 +7,16 @@ public class BounceWall : MonoBehaviour
     public Vector3 playerDirection;
     public Vector3 normalizedWall;
     public SpinnerControler spinerControler;
+    public Vector3 playerRotation;
     void OnEnable()
     {
         //Debug.Log("A " + spinerControler.moveDir);
         spinerControler.moveDir = Vector3.Reflect(playerDirection, normalizedWall);
+        playerRotation = spinerControler.GetComponentInParent<PlayerManager>().transform.rotation.eulerAngles;
+        Vector3 yes = Vector3.Reflect(playerRotation, normalizedWall);
+        Debug.Log("reflected vector : " + yes);
+        spinerControler.GetComponentInParent<PlayerManager>().transform.rotation = new Quaternion(yes.x, yes.y, yes.z, spinerControler.GetComponentInParent<PlayerManager>().transform.rotation.w);
+        Debug.Log("rotation now :" + spinerControler.GetComponentInParent<PlayerManager>().transform.rotation.eulerAngles);
         spinerControler.walled = true;
         //Debug.Log("B " +spinerControler.moveDir);
         
