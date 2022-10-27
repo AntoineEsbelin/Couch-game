4fd8f6e18809e9ed73f6457a05ca89edf89e9875
AkuroP <thomas.kang2002@gmail.com>
2022-10-27 16:02:10 +0200

    Point Zone modified for field

--- Couch Game/Assets/Scripts/Thomas/PointZone.cs
+++ Couch Game/Assets/Scripts/Thomas/PointZone.cs
@@ -6,51 +6,69 @@ using UnityEngine.InputSystem;
 public class PointZone : MonoBehaviour
 {
     [SerializeField] private int pointGiven;
+    [SerializeField] private bool isField;
 
     private void OnTriggerEnter(Collider coll)
     {
         if(coll.CompareTag("Player"))
         {
+            if(isField)return;
             PlayerManager deadPlayer = coll.GetComponentInParent<PlayerManager>();
-            if(deadPlayer.normalPlayer.activeSelf)
-            {
-                deadPlayer.normalPlayer.SetActive(false);
-                deadPlayer.normalPlayer.GetComponent<NormalControler>().enabled = false;
-            }
-            else if(deadPlayer.spinnerPlayer.activeSelf)
-            {
-                deadPlayer.spinnerPlayer.SetActive(false);
-                deadPlayer.spinnerControler.GetComponent<SpinnerControler>().enabled = false;
-            }
-            deadPlayer.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            deadPlayer.cameraTarget.targets.Remove(deadPlayer.transform);
-            //Give a certain amount of point at the last player touched
-            if(deadPlayer.lastPlayerContacted != null)
-            {
-                deadPlayer.lastPlayerContacted.playerPoint += pointGiven;
-            
-                //DEBUG
-                Debug.Log($"{deadPlayer.name} EJECTED !");
-                Debug.Log($"GIVE {pointGiven} points to {deadPlayer.lastPlayerContacted.name}");
+            DispawnPlayer(deadPlayer);
+        }
+    }
 
-                if(GameManager.instance.drawTimer)
+    private void OnTriggerExit(Collider coll)
+    {
+        if(coll.CompareTag("Player"))
+        {
+            if(!isField)return;
+            PlayerManager deadPlayer = coll.GetComponentInParent<PlayerManager>();
+            DispawnPlayer(deadPlayer);
+
+        }
+    }
+
+
+    private void DispawnPlayer(PlayerManager deadPlayer)
+    {
+        if(deadPlayer.normalPlayer.activeSelf)
+        {
+            deadPlayer.normalPlayer.SetActive(false);
+            deadPlayer.normalPlayer.GetComponent<NormalControler>().enabled = false;
+        }
+        else if(deadPlayer.spinnerPlayer.activeSelf)
+        {
+            deadPlayer.spinnerPlayer.SetActive(false);
+            deadPlayer.spinnerControler.GetComponent<SpinnerControler>().enabled = false;
+        }
+        deadPlayer.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        deadPlayer.cameraTarget.targets.Remove(deadPlayer.transform);
+        //Give a certain amount of point at the last player touched
+        if(deadPlayer.lastPlayerContacted != null)
+        {
+            deadPlayer.lastPlayerContacted.playerPoint += pointGiven;
+        
+            //DEBUG
+            Debug.Log($"{deadPlayer.name} EJECTED !");
+            Debug.Log($"GIVE {pointGiven} points to {deadPlayer.lastPlayerContacted.name}");
+
+            if(GameManager.instance.drawTimer)
+            {
+                if(deadPlayer.lastPlayerContacted.playerPoint > GameManager.instance.drawMaxPoint)
                 {
-                    if(deadPlayer.lastPlayerContacted.playerPoint > GameManager.instance.drawMaxPoint)
-                    {
-                        Debug.Log($"{deadPlayer.lastPlayerContacted.name} WINNN");
-                        GameManager.instance.timeOut = true;
-                        //STOP THE ROUND
-                    }
+                    Debug.Log($"{deadPlayer.lastPlayerContacted.name} WINNN");
+                    GameManager.instance.timeOut = true;
+                    //STOP THE ROUND
                 }
             }
-            else
-            {
-                //DEBUG
-                Debug.Log($"{deadPlayer.name} SUICIDED !");
-            }
-            deadPlayer.GetComponent<PlayerRespawn>().enabled = true;
-            deadPlayer.enabled = false;
-                
         }
+        else
+        {
+            //DEBUG
+            Debug.Log($"{deadPlayer.name} SUICIDED !");
+        }
+        deadPlayer.GetComponent<PlayerRespawn>().enabled = true;
+        deadPlayer.enabled = false;
     }
 }
