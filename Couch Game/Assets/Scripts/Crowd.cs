69d3c4a1c82f1e07514ea8ffc2736b0d67add40d
M0uftchup <julien.barlet34@gmail.com>
2022-12-08 17:01:47 +0100

    Crowd cheering
    Crowd cheering to every goal now

--- Couch Game/Assets/Scripts/Crowd.cs
+++ Couch Game/Assets/Scripts/Crowd.cs
@@ -12,9 +12,9 @@ public class Crowd : MonoBehaviour
     }
     public IEnumerator CrowdCheer()
     {
+        botAnim.SetBool("Cheering", true);
         float randomCheerTime = Random.Range(GameManager.instance.cheerMinTime, GameManager.instance.cheerMaxTime);
         yield return new WaitForSeconds(randomCheerTime);
-        botAnim.ResetTrigger("Cheer");
-        botAnim.SetTrigger("Cheer");
+        botAnim.SetBool("Cheering", false);
     }
 }
