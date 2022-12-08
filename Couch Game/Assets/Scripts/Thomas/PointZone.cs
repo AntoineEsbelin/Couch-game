910f6b53c69b520065668261b912d495f8a1b75a
Romain <mromain911@gmail.com>
2022-12-08 23:16:02 +0100

    bindingMenu

--- Couch Game/Assets/Scripts/Thomas/PointZone.cs
+++ Couch Game/Assets/Scripts/Thomas/PointZone.cs
@@ -65,6 +65,7 @@ public class PointZone : MonoBehaviour
           {
               if(controller.GetDevice<Gamepad>() == null)return;
               Gamepad gamePad = controller.GetDevice<Gamepad>();
+              
 
               if (gamePad == Gamepad.current)
               {
