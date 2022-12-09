41c5cfcd2f34c45459a7412f0f7028a3e4008b6a
M0uftchup <julien.barlet34@gmail.com>
2022-12-09 13:36:19 +0100

    Lights clignoting in menu

---
+++ Couch Game/Assets/RandomLight.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomLight : MonoBehaviour
+{
+    public List<Sprite> lights;
+    public Image lightOff;
+
+    public void changeSprite()
+    {
+        var randomSprite = Random.Range(0, 2);
+        Debug.Log("changed" + randomSprite);
+        lightOff.sprite = lights[randomSprite];
+    }
+}
