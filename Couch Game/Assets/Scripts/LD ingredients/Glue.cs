de7795e0113a29404205a55ccbd1a4ffcb06076a
AkuroP <thomas.kang2002@gmail.com>
2022-12-11 17:57:56 +0100

    Ice & Glue return player to normal

--- Couch Game/Assets/Scripts/LD ingredients/Glue.cs
+++ Couch Game/Assets/Scripts/LD ingredients/Glue.cs
@@ -6,6 +6,8 @@ public class Glue : MonoBehaviour
 {
     public float glueSpeedModifier = 0.2f;
     public float glueSpeedModifierSpinner = 0.6f;
+
+    private List<PlayerController> playersInGlue = new List<PlayerController>();
     
     void OnTriggerEnter(Collider other)
     {
@@ -14,6 +16,7 @@ public class Glue : MonoBehaviour
             PlayerController pCtrl = other.GetComponent<PlayerController>();
             pCtrl.NormalState.mSettings.glueSpeedModifier = glueSpeedModifier;
             pCtrl.SpinnerState.mSettings.glueSpeedModifier = glueSpeedModifierSpinner;
+            if(!playersInGlue.Contains(pCtrl))playersInGlue.Add(pCtrl);
         }
     }
 
@@ -22,8 +25,25 @@ public class Glue : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             PlayerController pCtrl = other.GetComponent<PlayerController>();
-            pCtrl.NormalState.mSettings.glueSpeedModifier = 1f;
-            pCtrl.SpinnerState.mSettings.glueSpeedModifier = 1f;
+            ReturnPlayerToNormal(pCtrl);
+        }
+    }
+
+    private void ReturnPlayerToNormal(PlayerController player)
+    {
+        player.NormalState.mSettings.glueSpeedModifier = 1f;
+        player.SpinnerState.mSettings.glueSpeedModifier = 1f;
+        if(playersInGlue.Contains(player))playersInGlue.Remove(player);
+
+    }
+
+    private void OnDisable()
+    {
+        if(playersInGlue.Count <= 0)return;
+
+        for(int i = 0; i <= playersInGlue.Count;)
+        {
+            ReturnPlayerToNormal(playersInGlue[i]);
         }
     }
 }
--- Couch Game/Assets/Scripts/LD ingredients/Ice.cs
+++ Couch Game/Assets/Scripts/LD ingredients/Ice.cs
@@ -9,6 +9,8 @@ public class Ice : MonoBehaviour
 
     public float spinnerTurnSmoothTimeModifier = 0.2f;
 
+    private List<PlayerController> playersInIce = new List<PlayerController>();
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -19,6 +21,7 @@ public class Ice : MonoBehaviour
             //if(pCtrl.startCharging)pCtrl.NormalState.SetSpeedModifier(0.9f);
             pCtrl.NormalState.mSettings.rotationSpeedModifier = iceRotationSpeedModifier;
             pCtrl.SpinnerState.mSettings.turnSmoothTimeModifier = spinnerTurnSmoothTimeModifier;
+            if(!playersInIce.Contains(pCtrl))playersInIce.Add(pCtrl);
         }
     }
 
@@ -27,11 +30,27 @@ public class Ice : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             PlayerController pCtrl = other.GetComponent<PlayerController>();
-            pCtrl.NormalState.mSettings.dxModifier = 1f;
-            pCtrl.NormalState.mSettings.slowSpeedModifier = .5f;
-            if(pCtrl.startCharging)pCtrl.NormalState.SetSpeedModifier(0.5f);
-            pCtrl.NormalState.mSettings.rotationSpeedModifier = 1f;
-            pCtrl.SpinnerState.mSettings.turnSmoothTimeModifier = 1f;
+            ReturnPlayerToNormal(pCtrl);
+        }
+    }
+
+    private void ReturnPlayerToNormal(PlayerController pCtrl)
+    {
+        pCtrl.NormalState.mSettings.dxModifier = 1f;
+        pCtrl.NormalState.mSettings.slowSpeedModifier = .5f;
+        if(pCtrl.startCharging)pCtrl.NormalState.SetSpeedModifier(0.5f);
+        pCtrl.NormalState.mSettings.rotationSpeedModifier = 1f;
+        pCtrl.SpinnerState.mSettings.turnSmoothTimeModifier = 1f;
+        if(playersInIce.Contains(pCtrl))playersInIce.Remove(pCtrl);
+    }
+
+    private void OnDisable()
+    {
+        if(playersInIce.Count <= 0)return;
+
+        for(int i = 0; i <= playersInIce.Count;)
+        {
+            ReturnPlayerToNormal(playersInIce[i]);
         }
     }
 }
